0c97664a5ee1fd44772fd9291389f062c6d118fb
Elena Karsten <e.karstenzv@gmail.com>
2023-05-19 18:31:47 +0800

    init

---
+++ src/js/GameController.js
@@ -0,0 +1,31 @@
+export default class GameController {
+  constructor(gamePlay, stateService) {
+    this.gamePlay = gamePlay;
+    this.stateService = stateService;
+  }
+
+  init() {
+    const currentLevel = 1;
+
+    const theme = themes[currentLevel];
+    this.gamePlay.drawUi(theme);
+
+    this.gamePlay.addCellClickListener(this.onCellClick);
+    this.gamePlay.addCellEnterListener(this.onCellEnter);
+    this.gamePlay.addCellLeaveListener(this.onCellLeave);
+
+    this.loadGame();
+  }
+
+  onCellClick(index) {
+    // TODO: react to click
+  }
+
+  onCellEnter(index) {
+    // TODO: react to mouse enter
+  }
+
+  onCellLeave(index) {
+    // TODO: react to mouse leave
+  }
+}
